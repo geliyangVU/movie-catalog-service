a25ed374e4698427542a8df85142aa62c413cc00
Geli Yang <gyang5@villanova.edu>
2022-10-07 11:38:24 -0400

    Update MovieCatalogResource.java
    Set up the getCatalog endpoint. Tested using postman

--- movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,14 +2,21 @@ package com.microservices_Geli.moviecatalogservice.resources;
 
 
 import com.microservices_Geli.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    public List<CatalogItem> getCatalog(String userId){
-        return null;
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Transformers action movie",4)
+        );
     }
 }
