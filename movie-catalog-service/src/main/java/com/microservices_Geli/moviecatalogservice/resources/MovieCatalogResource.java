8d7d284193171428338892044c8b71a37b509730
Geli Yang <gyang5@villanova.edu>
2022-10-22 13:16:36 -0400

    Make api calls to movie  info service using resttemplate
    Used getForObject method from RestTemplate to make api call. Mapped the response into Movie object. Imported Movie class inside models folder

--- movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,33 +2,39 @@ package com.microservices_Geli.moviecatalogservice.resources;
 
 
 import com.microservices_Geli.moviecatalogservice.models.CatalogItem;
+import com.microservices_Geli.moviecatalogservice.models.Movie;
 import com.microservices_Geli.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    List<Rating> ratings = Arrays.asList(
-            new Rating("1234",4),
-            new Rating("5678",3)
-            );
-
-
     //step 1, get all rated movie IDs, hard code ratings for now
     //step 2, for each movie id, call movie info service and get details
+
     //put them all together
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Transformers action movie",4)
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject("http://localhost:/8081/movies/foo", Movie.class);
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3)
         );
+
+        return ratings.stream().map(rating ->
+            new CatalogItem("Transformers", "Transformers action movie",4)
+
+        ).collect(Collectors.toList());
     }
 }
