393b7b42050d5cc64b347b2ef1403522e4883e61
Geli Yang <gyang5@villanova.edu>
2022-10-22 13:03:09 -0400

    Simulate api calls for RestTemplate
    Hard code ratings list in step1. To be implemented: resttemplate api call

--- movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,16 +2,28 @@ package com.microservices_Geli.moviecatalogservice.resources;
 
 
 import com.microservices_Geli.moviecatalogservice.models.CatalogItem;
+import com.microservices_Geli.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    List<Rating> ratings = Arrays.asList(
+            new Rating("1234",4),
+            new Rating("5678",3)
+            );
+
+
+    //step 1, get all rated movie IDs, hard code ratings for now
+    //step 2, for each movie id, call movie info service and get details
+    //put them all together
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
