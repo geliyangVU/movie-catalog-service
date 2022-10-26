3b39b6c43fe6f90c275f8d160e38df03827dab07
Geli Yang <gyang5@villanova.edu>
2022-10-26 11:46:26 -0400

    Solved a exception: InvalidDefinitionException by adding a non-argument constructor. Verified that api call was successful
    Detailed error messages:
    com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `com.microservices_Geli.moviecatalogservice.models.Movie` (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)

--- movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/models/Movie.java
+++ movie-catalog-service/src/main/java/com/microservices_Geli/moviecatalogservice/models/Movie.java
@@ -5,6 +5,9 @@ public class Movie {
     private String movieId;
     private String name;
 
+    public Movie() {
+    }
+
     public Movie(String movieId, String name) {
         this.movieId = movieId;
         this.name = name;
